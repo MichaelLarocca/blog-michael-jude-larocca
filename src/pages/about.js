57849d1bc02965a5df115e048df0183339039619
Michael Jude Larocca <michaeljudelarocca@gmail.com>
2020-11-29 21:17:31 -0500

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { FaCheckCircle } from "react-icons/fa"
+import "./index.css"
+
+import Sidebar from "../components/sidebar/Sidebar"
+import TechTag from "../components/tags/TechTag"
+
+const AboutPage = props => {
+  const labels = props.data.site.siteMetadata.labels
+  const aboutTags = ["react", "nodejs", "html", "css"]
+  const tags = {}
+  labels.forEach(label => {
+    aboutTags.forEach(tag => {
+      if (tag === label.tag) {
+        tags[tag] = label.name
+      }
+    })
+  })
+
+  return (
+    <Layout>
+      <SEO title="About" />
+      <div className="post-page-main">
+        <div className="sidebar px-4 py-2">
+          <Sidebar />
+        </div>
+
+        <div className="post-main">
+          <SEO title="About" />
+          <div className="mt-3">
+            <h2 className="heading">About</h2>
+            <p>
+              This blog documents the self-taught coding journey of a Generation X programmer.",
+            </p>
+            <br />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query aboutQuery {
+    site {
+      siteMetadata {
+        labels {
+          tag
+          tech
+          name
+          size
+          color
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
